70e133b81a9147f4d0eae8050aafc52653cc568b
Stefan Cosic <stefan22bl@gmail.com>
2022-07-03 06:47:22 +0200

    ViewsAndUrls

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,5 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 
 # Create your views here.
+def simple_view(request):
+    return HttpResponse("SIMPLE VIEW")
