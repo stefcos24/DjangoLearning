5eb0a6b2dbcab044a334c3762a31e1f1fdc65aea
Stefan Cosic <stefan22bl@gmail.com>
2022-07-04 05:31:37 +0200

    Redirects Basics

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.simple_view)
+    path('<int:num_page>', views.num_page_view),
+    path('<str:topic>/', views.news_view),
+    path('<int:num1>/<int:num2>', views.add_view)
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,7 +1,36 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("SIMPLE VIEW")
+
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'
+}
+
+def news_view(request, topic):
+    try:
+        result = articles[topic]
+        return HttpResponse(result)
+    except:
+        result = 'No page for that topic!'
+        return HttpResponseNotFound(result)
+        #raise Http404('Page not found!')
+
+def add_view(request, num1, num2):
+    #domain.com/my_app/num1/num2 --> result
+    add_result = num1 + num2
+    result = f"{num1}+{num2} = {add_result}"
+    return HttpResponse(str(result))
+
+# domain.com/my_app/0 ---> domain.com/my_app/sports
+
+def num_page_view(request, num_page):
+    try:
+        topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
+        topic = topics_list[num_page]
+
+        return HttpResponseRedirect(topic)
+    except:
+        raise Http404('Out of range of index')
