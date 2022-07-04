c0fb4f7e44a893098cce6922745517b883c040f2
Stefan Cosic <stefan22bl@gmail.com>
2022-07-04 15:25:18 +0200

    TagsIfElseFor

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def example_view(request):
     # my_app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+
+    my_var = {'first_name': 'Rosalind', 'last_name': 'Franklin', 'some_list': [1,2,3], 'user_logged_in': True}
+    return render(request, 'my_app/variable.html', context=my_var)
