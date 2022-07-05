da29258d9b14813fc05c8508e2c894920526536b
Stefan Cosic <stefan22bl@gmail.com>
2022-07-05 06:42:02 +0200

    StaticFiles

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# my_app/static/my_app/django.jpg
 STATIC_URL = 'static/'
 
 # Default primary key field type
