e544cfd5fab5322235eb078778cc8cccb6698d24
Stefan Cosic <stefan22bl@gmail.com>
2022-07-08 06:43:42 +0200

    Added Admin panel

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
+from pyexpat import model
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    context = {'all_cars': all_cars}
+    return render(request, 'cars/list.html', context=context)
 
 
 def add(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand, year=year)
+        # if user submitted new car --> List
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        # delete the car
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!')
+            return redirect(reverse('car:list'))
+    else:
+        return render(request, 'cars/delete.html')
