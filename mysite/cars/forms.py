d7c788259bca213af84d93ddbd8a19d608b2b6ad
Stefan Cosic <stefan22bl@gmail.com>
2022-07-09 12:30:08 +0200

    Added Djangoforms

--- mysite/cars/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Please write your review here', widget=forms.Textarea(attrs={'class': 'myform', 'rows': '2', 'cols': '2'}))
