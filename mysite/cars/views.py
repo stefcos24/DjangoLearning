3f21ca3d40ad2f983a50db45d9c3bd0c4d51f97b
Stefan Cosic <stefan22bl@gmail.com>
2022-07-09 12:29:01 +0200

    Added forms

--- mysite/cars/views.py
+++ mysite/cars/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ReviewForm
 
 # Create your views here.
 def rental_review(request):
-    return render(request, 'cars/rental_review.html')
+
+    # POST REQUEST --> FORM CONTENTS --> THANK YOU
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            # {'first_name': 'Jose'}
+            print(form.cleaned_data)
+            return redirect(reverse('cars:thank_you'))
+    # ELSE, RENDER FORM
+    else:
+        form = ReviewForm()
+
+    return render(request, 'cars/rental_review.html', context={'form': form})
 
 def thank_you(request):
     return render(request, 'cars/thank_you.html')
