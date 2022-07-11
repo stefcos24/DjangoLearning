6efcc863e5cd8b54ea4af0728b1ed2ebcbac690c
Stefan Cosic <stefan22bl@gmail.com>
2022-07-11 05:20:19 +0200

    Revert "Added forms"
    This reverts commit 3f21ca3d40ad2f983a50db45d9c3bd0c4d51f97b.

--- mysite/cars/views.py
+++ mysite/cars/views.py
@@ -1,23 +1,8 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from .forms import ReviewForm
+from django.shortcuts import render
 
 # Create your views here.
 def rental_review(request):
-
-    # POST REQUEST --> FORM CONTENTS --> THANK YOU
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            # {'first_name': 'Jose'}
-            print(form.cleaned_data)
-            return redirect(reverse('cars:thank_you'))
-    # ELSE, RENDER FORM
-    else:
-        form = ReviewForm()
-
-    return render(request, 'cars/rental_review.html', context={'form': form})
+    return render(request, 'cars/rental_review.html')
 
 def thank_you(request):
     return render(request, 'cars/thank_you.html')
